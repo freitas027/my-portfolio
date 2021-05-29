0be4bc67348ec9d32b5fdec934a828c0784d364b
Filipe Freitas <filipe.freitas.p@gmail.com>
2021-05-29 20:20:16 -0300

    Added dark theme and other improvements
    Added dark theme toogle
    
    Added elevation boxes to highlight content
    
    Fixed clock to always display 2 digits
    
    cleaned up margin and padding

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-let showing = false;
+const checkbox = document.querySelector("#checkbox");
 const contactBox = document.querySelector("#contact-box");
-console.log(contactBox);
+const themeMsg = document.querySelector("#theme-msg")
+
+let theme = localStorage.getItem('current-theme');
+const setDarkTheme = () => {
+    document.documentElement.setAttribute("current-theme", "dark")
+    localStorage.setItem("current-theme", "dark");
+}
+const setLightTheme = () => {
+    document.documentElement.setAttribute("current-theme", "light");
+    localStorage.setItem("current-theme", "light");
+}
+
+if (theme==='dark'){
+    setDarkTheme();
+    checkbox.checked = true;
+    themeMsg.innerHTML = "Night Mode On  ";
+}else{
+    setLightTheme();
+    checkbox.checked = false;
+    themeMsg.innerHTML = "Night Mode Off";
+}
+
+let showing = false;
+
 
 let animationDuration = getComputedStyle(contactBox).animationDuration.replace("s", "");
 animationDuration = (+animationDuration*1.2)*1000;
@@ -26,6 +49,17 @@ document.querySelector("#btn-contact").addEventListener('click', () =>{
         fadeOut();
     }
 });
+const body = document.querySelector("body");
+
+document.querySelector("#theme-toogle").addEventListener('click', ()=>{
+    if (checkbox.checked){
+        setDarkTheme();
+        themeMsg.innerHTML = "Night Mode On  ";
+    }else{
+        setLightTheme();
+        themeMsg.innerHTML = "Night Mode Off";
+    }
+});
 
 document.body.addEventListener('click', (event)=>{
     if(event.target.id != "btn-contact" && event.target.id != "contact-box"){
@@ -37,6 +71,13 @@ document.body.addEventListener('click', (event)=>{
 const clock = document.querySelector("#clock");
 setInterval(()=>{
     today = new Date(Date.now());
-    const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+    const time = `Time: ${localeTwoDigits(today.getHours())}:${localeTwoDigits(today.getMinutes())}:${localeTwoDigits(today.getSeconds())}`
     clock.innerHTML = time;
 }, 200);
+
+function localeTwoDigits (number){
+    return number.toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+    });
+}
